6c94fd9b02b6c1eff9564ab1c456a86029fda0eb
aldair xd <herreraaldair111@gmail.com>
2026-05-21 10:27:57 -0500

    avances

--- app/src/main/java/com/edu/uteq/MainActivity.java
+++ app/src/main/java/com/edu/uteq/MainActivity.java
@@ -1,6 +1,9 @@
 package com.edu.uteq;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnIr = findViewById(R.id.btnEntrar);
+
+        btnIr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
